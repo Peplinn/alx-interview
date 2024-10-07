35ec5dfec2b40040acec9353333313874d9a42e1
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-10-07 02:53:05 +0100

    Pascal Triangle Files

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/pvaluethon3
+"""A script to determine pascal's triangle for anvalue number"""
+
+
+def pascal_triangle(num) :
+    """
+    returns list of lists containing Pascal’s triangle integers of n
+    """
+    triangle = []
+
+    if num <= 0 :
+        return triangle
+
+    if num == 1 :
+        triangle.append(1)
+
+
+    for row in range(num) :
+        # print("This iteration, row is ", row)
+        row_list = []
+
+        for value in range(row+1):
+            if value in (0, row):
+                row_list.append(1)
+            else:
+                row_list.append(triangle[row-1][value-1] + triangle[row-1][value])
+
+        triangle.append(row_list)
+
+    return triangle
---
+++ README.md
@@ -0,0 +1,1 @@
+The Repository for ALX Interview Tasks.
