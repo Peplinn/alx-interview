f39753062430200d0c06c312d3ebe2e02092d027
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-10-11 03:20:06 +0100

    Lockboxes Files

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''A module for working with lockboxes.
+'''
+
+def canUnlockAll(boxes):
+    '''Given that the first box is open, and every key
+    opens the door whose number it bears,
+    this checks if all the boxes
+    in the list can be opened by the keys found.
+    '''
+    opened = [False] * len(boxes)
+    opened[0] = True
+    keys = boxes[0]
+
+    for key in keys:
+        if 0 <= key < len(boxes) and not opened[key]:
+            opened[key] = True
+            keys.extend(boxes[key])
+
+    return all(opened)
