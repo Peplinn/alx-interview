5ae530baed8eb907774863f33e0d4c707aa15be2
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-10-17 23:49:45 +0100

    Initial Commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Method that calculates fewest number of operations needed
+to result in 
+"""
+
+def minOperations(n):
+    """
+    In a text file with a single character,
+    this checks whether only Copy All and Paste can yield
+    exactly n 'H' characters in a given file.
+    """
+    stringH = ""
+    clipboard = ""
+
+    
+
+
+
+
