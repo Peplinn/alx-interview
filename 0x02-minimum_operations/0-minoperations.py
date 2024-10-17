198a39ba9644bfe33a25beeeb3c60d479e5b7e80
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-10-18 00:38:57 +0100

    Task Solution

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Method that calculates fewest number of operations needed
-to result in 
+to result in
 """
 
 def minOperations(n):
@@ -10,11 +10,37 @@ def minOperations(n):
     this checks whether only Copy All and Paste can yield
     exactly n 'H' characters in a given file.
     """
-    stringH = ""
+    printedH = "H"
     clipboard = ""
+    operationsPerformed = 0
 
-    
+    while len(printedH) <= n:
+        if n % 2 and operationsPerformed == 2:
+            printedH += "H"
+            clipboard = "H"
+            operationsPerformed += 1
+            print("Clipboard : " + clipboard)
+            print(printedH)
+            print(operationsPerformed)
 
+        if len(printedH) + len(clipboard) <= n:
+            if len(clipboard) < len(printedH) and 2 * len(printedH) <= n:
+                clipboard = printedH # Copy
+                printedH += clipboard # Paste
+                operationsPerformed += 2
+                print("Clipboard : " + clipboard)
+                print(printedH)
+                print(operationsPerformed)
+            elif (
+                len(printedH) < len(clipboard)
+                or len(printedH) + len(clipboard) == n
+            ):
+                printedH += clipboard
+                operationsPerformed += 1
+                print("Clipboard : " + clipboard)
+                print(printedH)
+                print(operationsPerformed)
+        else:
+            break
 
-
-
+    return operationsPerformed
