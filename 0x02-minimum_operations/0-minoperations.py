8dfdcfd4bbe2aeacdc89ab18b63522f22bf9add1
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-10-18 01:15:48 +0100

    More Edge Case Fixes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -23,19 +23,38 @@ def minOperations(n):
             printedH += "H"
             clipboard = "H"
             operationsPerformed += 1
+            print("Clipboard : " + clipboard)
+            print(printedH)
+            print(operationsPerformed)
 
         if len(printedH) + len(clipboard) <= n:
             if len(clipboard) < len(printedH) and 2 * len(printedH) <= n:
                 clipboard = printedH
                 printedH += clipboard
                 operationsPerformed += 2
+                print("Clipboard : " + clipboard)
+                print(printedH)
+                print(operationsPerformed)
             elif (
                 len(printedH) < len(clipboard) or
-                len(printedH) + len(clipboard) == n
+                len(printedH) + len(clipboard) == n or
+                (len(printedH) + len(clipboard) <= n and
+                 not (n % len(clipboard)))
             ):
                 printedH += clipboard
                 operationsPerformed += 1
+                print("Clipboard : " + clipboard)
+                print(printedH)
+                print(operationsPerformed)
+            else:
+                break
         else:
             break
+    
+    print("PrintedH: " + printedH)
+    print("Clipboard: " + clipboard)
+
+    if len(printedH) != n:
+        return 0
 
     return operationsPerformed
