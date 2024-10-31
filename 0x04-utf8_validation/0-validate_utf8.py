98c983ace475f18c0d34291c26dc9cd6089b2208
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-10-31 23:38:01 +0100

    Initial Commit; UTF-8 Project Files

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+A method that determines if a given data set
+represents a valid UTF-8 encoding.
+
+Prototype: def validUTF8(data)
+Return: True if data is a valid UTF-8 encoding, else
+return False
+A character in UTF-8 can be 1 to 4 bytes long
+The data set can contain multiple characters
+The data will be represented by a list of integers
+Each integer represents 1 byte of data, therefore you
+only need to handle the 8 least significant bits of each
+integer
+"""
+
+def validUTF8(data):
+    """_summary_
+
+    Args:
+            data (list[int]): a list of integers
+    """
+    num_bytes = 0
+
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
+    for byte in data:
+        byte &= 0xFF
+
+        if num_bytes == 0:
+            mask = 1 << 7
+            while mask & byte:
+                num_bytes += 1
+                mask = mask >> 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+
+        num_bytes -= 1
+
+    return num_bytes == 0
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+# Solution to the UTF-8 Validation Task
