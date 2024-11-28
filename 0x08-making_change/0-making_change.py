1612e68443ebad233dae6d4701efbf7dd4897ffa
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-11-28 20:02:44 +0100

    Making Change Task Solution

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+
+def makeChange(coins, total):
+    """_summary_
+
+    Args:
+                    coins (_type_): _description_
+                    total (_type_): _description_
+    """
+    sorted_coins = sorted(coins, reverse=True)
+    operation_count = 0
+    amount_left = total
+
+    while amount_left > 0:
+        for coin in sorted_coins:
+            if coin > amount_left and amount_left != 0:
+                return -1
+            else:
+                divisions = amount_left // coin
+                amount_left = amount_left - (divisions * coin)
+                operation_count += divisions
+
+    return operation_count
