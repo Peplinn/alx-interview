35d6fcf8841285f9d40783d4d79aed78f97c1531
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-11-28 23:12:50 +0100

    Edge Case Fix

--- 0x08-making_change/0-main.py
+++ 0x08-making_change/0-main.py
@@ -10,3 +10,5 @@ print(makeChange([1, 2, 25], 0))
 print(makeChange([1256, 54, 48, 16, 102], 1453))
 
 print(makeChange([4, 5], 7))
+
+print(makeChange([9, 6, 1], 11))
--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,23 +7,23 @@ def makeChange(coins, total):
     """_summary_
 
     Args:
-                    coins (_type_): _description_
-                    total (_type_): _description_
+                    coins (list): List of coin denominations
+                    total (int): Target amount
     """
+    if total <= 0:
+        return 0
+
     sorted_coins = sorted(coins, reverse=True)
     operation_count = 0
     amount_left = total
 
-    while amount_left > 0:
-        for coin in sorted_coins:
-            if coin > amount_left and amount_left != 0:
-                return -1
-            else:
-                divisions = amount_left // coin
-                amount_left = amount_left - (divisions * coin)
-                operation_count += divisions
+    for coin in sorted_coins:
+        if amount_left == 0:
+            break
 
-    if amount_left > 0:
-        return -1
+        if coin <= amount_left:
+            divisions = amount_left // coin
+            amount_left -= divisions * coin
+            operation_count += divisions
 
-    return operation_count
+    return operation_count if amount_left == 0 else -1
